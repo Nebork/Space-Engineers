b95bdd7e97849a4fd541508320a853e573fe808c
Nebork <Nebork@yahoo.com>
2024-06-03 15:40:11 +0200

    Working script for all lights in a group, needs some cleanup and more comments.

--- Party_Light_Script/Party_Light_Script/Program.cs
+++ Party_Light_Script/Party_Light_Script/Program.cs
@@ -22,51 +22,37 @@ namespace IngameScript
 {
     partial class Program : MyGridProgram
     {
-        // This file contains your actual script.
-        //
-        // You can either keep all your code here, or you can create separate
-        // code files to make your program easier to navigate while coding.
-        //
-        // In order to add a new utility class, right-click on your project, 
-        // select 'New' then 'Add Item...'. Now find the 'Space Engineers'
-        // category under 'Visual C# Items' on the left hand side, and select
-        // 'Utility Class' in the main area. Name it in the box below, and
-        // press OK. This utility class will be merged in with your code when
-        // deploying your final script.
-        //
-        // You can also simply create a new utility class manually, you don't
-        // have to use the template if you don't want to. Just do so the first
-        // time to see what a utility class looks like.
-        // 
-        // Go to:
-        // https://github.com/malware-dev/MDK-SE/wiki/Quick-Introduction-to-Space-Engineers-Ingame-Scripts
-        //
-        // to learn more about ingame scripts.
-
         // COPY FROM HERE
 
-        Color color = new Color();
         Random rnd = new Random();
-        IMyLightingBlock light;
-        string name;
+        string groupName;
+        IMyBlockGroup partyGroup;
+        List<IMyLightingBlock> partyLights;
 
         public Program()
         {
             Runtime.UpdateFrequency = UpdateFrequency.Update1;
-            name = "[CRG] Rotating Light - Party Nebork";
-            light = GridTerminalSystem.GetBlockWithName(name) as IMyLightingBlock;
 
-            if (light == null)
+
+            // Takes all the lights from the group and puts them into the list "partyLights"
+            groupName = "[CRG] - Party";
+            partyGroup = GridTerminalSystem.GetBlockGroupWithName(groupName);
+            partyLights = new List<IMyLightingBlock>();
+            partyGroup.GetBlocksOfType<IMyLightingBlock>(partyLights);
+
+            foreach (var partyLight in partyLights)
+            {
+                partyLight.Color = RandomValidColor();
+            }
+
+
+            // CHANGE TO EMPTY GROUP AND NO LIGHTS IN GROUP
+            /*if (light == null)
             {
                 Echo($"Could not find light with name {name}");
                 Runtime.UpdateFrequency = UpdateFrequency.None;
                 return;
-            }
-
-            color.R = 255;
-            color.G = 0;
-            color.B = 255;
-            light.Color = color;
+            }*/
         }
 
         public Color NextColor(Color startColor)
@@ -106,27 +92,59 @@ namespace IngameScript
             else return startColor;
         }
 
+        public Color RandomValidColor()
+        {
+            int randomNmbr = rnd.Next(6);
+
+            byte x = (byte) rnd.Next(256);
+
+            Color rndColor = new Color();
+
+            switch(randomNmbr)
+            {
+                case 0:
+                    rndColor.R = x;
+                    rndColor.G = 0;
+                    rndColor.B = 255;
+                    break;
+                case 1:
+                    rndColor.R = 0;
+                    rndColor.G = x; ;
+                    rndColor.B = 255;
+                    break;
+                case 2:
+                    rndColor.R = 0;
+                    rndColor.G = 255;
+                    rndColor.B = x; ;
+                    break;
+                case 3:
+                    rndColor.R = x; ;
+                    rndColor.G = 255;
+                    rndColor.B = 0;
+                    break;
+                case 4:
+                    rndColor.R = 255;
+                    rndColor.G = x;
+                    rndColor.B = 0;
+                    break;
+                case 5:
+                    rndColor.R = 255;
+                    rndColor.G = 0;
+                    rndColor.B = x;
+                    break;
+            }
+
+            return rndColor;
+        }
+
         public void Main(string argument, UpdateType updateSource)
         {
-            // The main entry point of the script, invoked every time
-            // one of the programmable block's Run actions are invoked,
-            // or the script updates itself. The updateSource argument
-            // describes where the update came from. Be aware that the
-            // updateSource is a  bitfield  and might contain more than 
-            // one update type.
-            // 
-            // The method itself is required, but the arguments above
-            // can be removed if not needed.
-
-            // For random
-            // color.R = (byte)rnd.Next(256);
-            // color.G = (byte)rnd.Next(256);
-            // color.B = (byte)rnd.Next(256);
-
-            color = light.Color;
-            color = NextColor(color);
-            light.Color = color;
+            foreach (var partyLight in partyLights)
+            {
+                partyLight.Color = NextColor(partyLight.Color);
+            }
         }
+
         // COPY UNTIL HERE
     }
 }
